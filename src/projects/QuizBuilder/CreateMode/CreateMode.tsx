0a3fbb4879ffc29c1acaeacea3d4f945b03f9703
Etienne BLANC <etienne.blanc94@gmail.com>
2019-12-11 00:19:22 +0100

    maj CreateMode

--- src/projects/QuizBuilder/CreateMode/CreateMode.tsx
+++ src/projects/QuizBuilder/CreateMode/CreateMode.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+const Infos = () => {
+  return <p>Infos</p>;
+};
+
+const AddAnswersArea = () => {
+  return <p>Zone ajout questions</p>;
+};
+
+const Buttons = () => {
+  return <p>Boutons</p>;
+};
+
+const CreateQuiz = () => {
+  return (
+    <Container>
+      <Row>
+        <Infos />
+      </Row>
+      <Row>
+        <AddAnswersArea />
+      </Row>
+      <Row>
+        <Buttons />
+      </Row>
+    </Container>
+  );
+};
 
 export const CreateMode = () => {
-  return <p>CreateMode !</p>;
+  return <CreateQuiz />;
 };
